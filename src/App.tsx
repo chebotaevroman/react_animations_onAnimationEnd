d18cf73f9f2a46ca1c26298b14b7bfe794db11ee
Roman Chebotaev <roman23.fe@gmail.com>
2022-12-22 18:40:11 +0300

    done 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isCardVisible, setIsCardVisible] = useState(true);
+
+  const [isFadeIn, setIsFadeIn] = useState(false);
+  const [isFadeOut, setIsFadeOut] = useState(false);
+
+  const handleButtonClick = (newVisibility: boolean) => {
+    if (newVisibility) {
+      setIsCardVisible(true);
+      setIsFadeIn(true);
+    } else {
+      setIsFadeOut(true);
+    }
+  };
+
+  const handleAnimationEnd = (e: React.AnimationEvent<HTMLDivElement>) => {
+    if (e.animationName === "fadeIn") {
+      setIsFadeIn(false);
+    }
+
+    if (e.animationName === "fadeOut") {
+      setIsCardVisible(false);
+      setIsFadeOut(false);
+    }
+  };
+
+  const cardClassNames = ["card"];
+  if (isFadeIn) cardClassNames.push("cardFadeIn");
+  if (isFadeOut) cardClassNames.push("cardFadeOut");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="wrapper">
+          <button
+            className="button"
+            onClick={() => handleButtonClick(!isCardVisible)}
+          >
+            Show / Hide
+          </button>
+          {isCardVisible ? (
+            <div
+              className={cardClassNames.join(" ")}
+              onAnimationEnd={handleAnimationEnd}
+            >
+              Card
+            </div>
+          ) : null}
+        </div>
       </header>
     </div>
   );
